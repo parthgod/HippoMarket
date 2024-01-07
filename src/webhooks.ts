4f59fa498187c020c288e593f28d9702194c5536
parthgod <parthgenius.gps@gmail.com>
2024-01-08 00:22:27 +0530

    made fixes-2

--- src/webhooks.ts
+++ src/webhooks.ts
@@ -3,9 +3,9 @@ import { WebhookRequest } from "./server";
 import { stripe } from "./lib/stripe";
 import Stripe from "stripe";
 import { getPayloadClient } from "./get-payload";
-// import { Product } from "./payload-types";
-// import nodemailer from "nodemailer";
-// import { ReceiptEmailHtml } from "./components/emails/ReceiptEmail";
+import { Product } from "./payload-types";
+import nodemailer from "nodemailer";
+import { ReceiptEmailHtml } from "./components/emails/ReceiptEmail";
 
 export const stripeWebhookHandler = async (req: express.Request, res: express.Response) => {
   const webhookRequest = req as any as WebhookRequest;
@@ -70,36 +70,37 @@ export const stripeWebhookHandler = async (req: express.Request, res: express.Re
     });
 
     // send receipt
-    // const transporter = nodemailer.createTransport({
-    //   service: "gmail",
-    //   host: "smtp.gmail.com",
-    //   port: 587,
-    //   secure: false,
-    //   auth: {
-    //     user: "parthgenius.gps@gmail.com",
-    //     pass: "szktlhaxuvxzehco",
-    //   },
-    // });
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: "parthgenius.gps@gmail.com",
+        pass: "szktlhaxuvxzehco",
+      },
+    });
 
-    // const mailOptions = {
-    //   from: "HippoMarket <rauvishnu7@gmail.com>",
-    //   to: user.email,
-    //   subject: "Thanks for your order! This is your receipt.",
-    //   text: "Email content",
-    //   html: ReceiptEmailHtml({
-    //     date: new Date(),
-    //     email: user.email,
-    //     orderId: session.metadata.orderId,
-    //     products: order.products as Product[],
-    //   }),
-    // };
+    const mailOptions = {
+      from: "HippoMarket <rauvishnu7@gmail.com>",
+      to: user.email,
+      subject: "Thanks for your order! This is your receipt.",
+      text: "Email content",
+      html: ReceiptEmailHtml({
+        date: new Date(),
+        email: user.email,
+        orderId: session.metadata.orderId,
+        products: order.products as Product[],
+      }),
+    };
 
-    // try {
-    //   const data = await transporter.sendMail(mailOptions);
-    //   res.status(200).json({ data });
-    // } catch (error) {
-    //   res.status(500).json({ error });
-    // }
+    try {
+      const data = await transporter.sendMail(mailOptions);
+      res.status(200).json({ data });
+      console.log(data);
+    } catch (error) {
+      res.status(500).json({ error });
+    }
   }
 
   return res.status(200).send();
